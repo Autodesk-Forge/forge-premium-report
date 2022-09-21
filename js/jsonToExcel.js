b1ac375813930d30cb3740fbcda042301d551bf0
deepalics0044 <deepalics0044@gmail.com>
2022-09-22 02:57:15 +0530

    update

--- js/jsonToExcel.js
+++ js/jsonToExcel.js
@@ -15,25 +15,26 @@
 // DOES NOT WARRANT THAT THE OPERATION OF THE PROGRAM WILL BE
 // UNINTERRUPTED OR ERROR FREE.
 /////////////////////////////////////////////////////////////////////
-document.getElementsByTagName("head")[0].innerHTML += '<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"><\/script>';
+document.getElementsByTagName('head')[0].innerHTML +=
+	'<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>';
 var createXLSLFormatObj = [];
-var xlsHeader = ["Email", "License Purchased"];
+var xlsHeader = ['Email', 'License Purchased'];
 createXLSLFormatObj.push(xlsHeader);
-$.each(active, function(index, value) {
-    var innerRowData = [];
-    $("tbody").append('<tr><td>' +email.value  + '</td><td>'+ seat_quantity.value + '</td></tr>');
-    $.each(value, function(ind, val) {
-        innerRowData.push(val);
-    });
-    createXLSLFormatObj.push(innerRowData);
+$.each(active, function (index, value) {
+	var innerRowData = [];
+	$('tbody').append('<tr><td>' + email.value + '</td><td>' + seat_quantity.value + '</td></tr>');
+	$.each(value, function (ind, val) {
+		innerRowData.push(val);
+	});
+	createXLSLFormatObj.push(innerRowData);
 });
 /* File Name */
-var filename = "license_assigned.xlsx";
+var filename = 'license_assigned.xlsx';
 /* Sheet Name */
-var ws_name = "license_assigned";
+var ws_name = 'license_assigned';
 if (typeof console !== 'undefined') console.log(new Date());
 var wb = XLSX.utils.book_new(),
-    ws = XLSX.utils.aoa_to_sheet(createXLSLFormatObj);
+	ws = XLSX.utils.aoa_to_sheet(createXLSLFormatObj);
 /* Add worksheet to workbook */
 XLSX.utils.book_append_sheet(wb, ws, ws_name);
 /* Write workbook and Download */
